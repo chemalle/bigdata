326fef3c649b2692e78c91ca32b31c8f64851414
Eduardo Chemalle <chemalle@econobilidade.com>
2018-10-02 13:53:20 -0300

    updated
    very importante things updates

--- ecoblog/posts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Post, Stocks, Input
-
-admin.site.register(Post)
-admin.site.register(Stocks)
-admin.site.register(Input)
